787aaea832890b48f2f656b76b87297715b3030a
getcerts1 <barrowtech1999@gmail.com>
2025-03-28 20:16:21 +0200

    created the networking components for the deployment of our architecture

---
+++ modules/networking/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.108.0"
+    }
+  }
+}
+resource "azurerm_virtual_network" "hub-vnet" {
+  address_space = [10.0.0.0/16]
+  location            = var.location
+  name                = var.hub-vnet-name
+  resource_group_name = var.rg-name
+}
+
+resource "azurerm_subnet" "app-gw-subnet" {
+  address_prefixes = [10.0.0.0/24]
+  name                 = var.ApplicationGatewaySubnet-001
+  resource_group_name  = var.rg-name
+  virtual_network_name = azurerm_virtual_network.hub-vnet
+}
+
+
+resource "azurerm_virtual_network" "spoke-vnet" {
+  address_space = [10.1.0.0/16]
+  location            = var.location
+  name                = var.spoke-vnet-name
+  resource_group_name = var.rg-name
+}
+
+resource "azurerm_subnet" "funcapp-subnet" {
+  address_prefixes = [10.1.0.0/24]
+  name                 = var.spoke-subnet-001
+  resource_group_name  = var.rg-name
+  virtual_network_name = azurerm_virtual_network.spoke-vnet.name
+}
+
+resource "azurerm_subnet" "private-endpoint-subnet" {
+  address_prefixes = [10.1.1.0/24]
+  name                 = var.spoke-subnet-002
+  resource_group_name  = var.rg-name
+  virtual_network_name = azurerm_virtual_network.spoke-vnet.name
+}
+
+resource "azurerm_virtual_network_peering" "peering-connection" {
+  name                      = "to-spoke"
+  remote_virtual_network_id = azurerm_virtual_network.spoke-vnet.id
+  resource_group_name       = var.rg-name
+  virtual_network_name      = azurerm_virtual_network.hub-vnet.name
+}
---
+++ modules/networking/variables.tf
@@ -0,0 +1,41 @@
+variable "rg-name" {
+  type = string
+}
+
+
+variable "location" {
+  type = string
+  default = "westus"
+}
+
+variable "hub-vnet-name" {
+  type = string
+  default = "hub-vnet-001"
+
+}
+
+variable "spoke-vnet-name" {
+  type = string
+  default = "spoke-vnet-001"
+}
+
+variable "ApplicationGatewaySubnet-001" {
+  type = string
+  default = "ApplicationGatewaySubnet"
+}
+
+variable "spoke-subnet-001" {
+  type = string
+  default = "funcapp-subnet"
+
+}
+
+variable "spoke-subnet-002" {
+  type = string
+  default = "PrivateEndpointSubnet-001"
+}
+
+
+
+
+
